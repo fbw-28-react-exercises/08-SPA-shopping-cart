30885d540399793ff26e7272b7e79643b4a47604
nrcool <nrcoolove@yahoo.com>
2020-09-09 14:12:38 +0200

    added comments

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,24 +16,32 @@ export default function Cart() {
 
   const removeOneItem = (item) => {
     //changing Cart Data
+    //first find the item user clicked in cartData array
     let searchItem = cartData.find(({ title }) => title === item.title);
+    //then find index number of that item
     let index = cartData.indexOf(searchItem);
-
+    //make a copy of cartData array,so can manipulate data (that item)
     let copyCartData = [...cartData];
 
+    //checking for the condition, if number of items in our cart is greater than one,we will only reduce number property of that item
     if (searchItem.number > 1) {
       copyCartData[index].number = copyCartData[index].number - 1;
       setCartData(copyCartData);
     } else {
+        //otherwise remove that specific item completely
       copyCartData.splice(index, 1);
       setCartData(copyCartData);
     }
 
     //changing in main Data
+    //finding that item in our main data,so can make increment back in our inventory
     let serachItemInData = data.find(({ title }) => title === item.title);
     let indexInData = data.indexOf(serachItemInData);
+    //making copy of main Data
     let copyData = [...data];
+    //targeting that specific item in our data and increment inventory property by 1.
     copyData[indexInData].inventory = copyData[indexInData].inventory + 1;
+    //once we have change our data ,we are setting our state.
     setData(copyData);
   };
 
@@ -44,6 +52,7 @@ export default function Cart() {
  
      let copyCartData = [...cartData];
 
+     //when user click on delete all button, here we dont need any condition ,we will delete complete item from our card.
        copyCartData.splice(index, 1);
        setCartData(copyCartData);
  
@@ -51,6 +60,7 @@ export default function Cart() {
      let serachItemInData = data.find(({ title }) => title === item.title);
      let indexInData = data.indexOf(serachItemInData);
      let copyData = [...data];
+     //setting our item inventory back to what is was before in main data.
      copyData[indexInData].inventory = copyData[indexInData].inventory + item.number;
      setData(copyData);
   }
